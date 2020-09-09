127eec05f96d6ba47b9f4d89f41db79002225df7
Michael Meidl <mmeidl@amazon.com>
2020-09-08 18:49:03 -0700

    Override fs.s3a.endpoint for AWS GovCloud regions
    Plus small fix for test_spark_history_server to use regionalized sagemaker_session from pytest fixture

--- Makefile
+++ Makefile
@@ -87,7 +87,8 @@ test-local: install-sdk build-tests
 # Use pytest-parallel to run tests in parallel - https://pypi.org/project/pytest-parallel/
 test-sagemaker: install-sdk build-tests
 	# https://github.com/ansible/ansible/issues/32499#issuecomment-341578864
-	OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES pytest --workers auto -s -vv test/integration/sagemaker --repo=$(DEST_REPO) --tag=$(VERSION) --durations=0 \
+	OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES pytest --workers auto -s -vv test/integration/sagemaker \
+	--repo=$(DEST_REPO) --tag=$(VERSION) --durations=0 \
 	--role $(ROLE) \
 	--image_uri $(IMAGE_URI) \
 	--region ${REGION} \
--- test/integration/sagemaker/test_spark_history_server.py
+++ test/integration/sagemaker/test_spark_history_server.py
@@ -7,7 +7,7 @@ from sagemaker.spark.processing import PySparkProcessor
 MAX_RETRIES = 10
 
 
-def test_history_server(tag, role, image_uri):
+def test_history_server(tag, role, image_uri, sagemaker_session):
     spark = PySparkProcessor(
         base_job_name="sm-spark",
         framework_version=tag,
@@ -16,15 +16,18 @@ def test_history_server(tag, role, image_uri):
         instance_count=1,
         instance_type="ml.c5.xlarge",
         max_runtime_in_seconds=1200,
+        sagemaker_session=sagemaker_session,
     )
-    bucket = spark.sagemaker_session.default_bucket()
+    bucket = sagemaker_session.default_bucket()
     spark_event_logs_key_prefix = "spark/spark-history-fs"
     spark_event_logs_s3_uri = "s3://{}/{}".format(bucket, spark_event_logs_key_prefix)
 
     with open("test/resources/data/files/sample_spark_event_logs") as data:
         body = data.read()
         S3Uploader.upload_string_as_file_body(
-            body=body, desired_s3_uri=spark_event_logs_s3_uri + "/sample_spark_event_logs"
+            body=body,
+            desired_s3_uri=spark_event_logs_s3_uri + "/sample_spark_event_logs",
+            sagemaker_session=sagemaker_session,
         )
 
     spark.start_history_server(spark_event_logs_s3_uri=spark_event_logs_s3_uri)
