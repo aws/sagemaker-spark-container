9b2e7282cea9329c8efd850a7975c5950d8bdb86
Michael Meidl <mmeidl@amazon.com>
2020-09-10 02:21:55 -0700

    Implement integ test for SSE-S3 encryption, refactor basic algo error integ test
    test_sagemaker_pyspark_sse_s3 test that Spark container can read and write S3 data encrypted with SSE-S3
    replace test_sagemaker_pyspark_algorithm_error with test_spark_app_error to more directly test a PySpark script failing with nonzero exit code

---
+++ test/integration/sagemaker/test_sagemaker_spark_errors.py
@@ -0,0 +1,32 @@
+from sagemaker.spark.processing import PySparkProcessor
+
+
+def test_spark_app_error(tag, role, image_uri, sagemaker_session):
+    """Submits a PySpark app which is scripted to exit with error code 1"""
+    spark = PySparkProcessor(
+        base_job_name="sm-spark-app-error",
+        framework_version=tag,
+        image_uri=image_uri,
+        role=role,
+        instance_count=1,
+        instance_type="ml.c5.xlarge",
+        max_runtime_in_seconds=1200,
+        sagemaker_session=sagemaker_session,
+    )
+
+    try:
+        spark.run(
+            submit_app_py="test/resources/code/python/py_spark_app_error/py_spark_app_error.py",
+            wait=True,
+            logs=False,
+        )
+    except Exception:
+        pass  # this job is expected to fail
+    processing_job = spark.latest_job
+
+    describe_response = processing_job.describe()
+    assert "AlgorithmError: See job logs for more information" == describe_response["FailureReason"]
+    assert "Algorithm Error: (caused by CalledProcessError)" in describe_response["ExitMessage"]
+    assert "returned non-zero exit status 1" in describe_response["ExitMessage"]
+
+
--- test/integration/sagemaker/test_spark.py
+++ test/integration/sagemaker/test_spark.py
@@ -79,32 +79,6 @@ def configuration() -> list:
     return configuration
 
 
-def test_sagemaker_pyspark_algorithm_error(tag, role, image_uri, sagemaker_session):
-    spark = PySparkProcessor(
-        base_job_name="sm-spark-py-customer-error",
-        framework_version=tag,
-        image_uri=image_uri,
-        role=role,
-        instance_count=1,
-        instance_type="ml.c5.xlarge",
-        max_runtime_in_seconds=1200,
-        sagemaker_session=sagemaker_session,
-    )
-
-    try:
-        spark.run(
-            submit_app_py="test/resources/code/python/hello_py_spark/hello_py_spark_app.py",
-            arguments=["invalid_arg"],
-            wait=True,
-            logs=False,
-        )
-    except Exception:
-        pass  # this job is expected to fail
-    processing_job = spark.latest_job
-
-    assert "Algorithm Error: (caused by CalledProcessError)" in processing_job.describe()["ExitMessage"]
-
-
 def test_sagemaker_pyspark_multinode(tag, role, image_uri, configuration, sagemaker_session, region, sagemaker_client):
     """Test that basic multinode case works on 32KB of data"""
     spark = PySparkProcessor(
@@ -174,6 +148,56 @@ def test_sagemaker_pyspark_multinode(tag, role, image_uri, configuration, sagema
 
     # Commenting this assert because it's flaky.
     # assert updated_times_count > 1
+
+    output_contents = S3Downloader.list(output_data_uri, sagemaker_session=sagemaker_session)
+    assert len(output_contents) != 0
+
+
+# TODO: similar integ test case for SSE-KMS. This would require test infrastructure bootstrapping a KMS key.
+# Currently, Spark jobs can read data encrypted with SSE-KMS (assuming the execution role has permission),
+# however our Hadoop version (2.8.5) does not support writing data with SSE-KMS (enabled in version 3.0.0).
+def test_sagemaker_pyspark_sse_s3(tag, role, image_uri, sagemaker_session, region, sagemaker_client):
+    """Test that Spark container can read and write S3 data encrypted with SSE-S3 (default AES256 encryption)"""
+    spark = PySparkProcessor(
+        base_job_name="sm-spark-py",
+        framework_version=tag,
+        image_uri=image_uri,
+        role=role,
+        instance_count=2,
+        instance_type="ml.c5.xlarge",
+        max_runtime_in_seconds=1200,
+        sagemaker_session=sagemaker_session,
+    )
+    bucket = sagemaker_session.default_bucket()
+    timestamp = datetime.now().isoformat()
+    input_data_key = f"spark/input/sales/{timestamp}/data.jsonl"
+    input_data_uri = f"s3://{bucket}/{input_data_key}"
+    output_data_uri = f"s3://{bucket}/spark/output/sales/{timestamp}"
+    s3_client = sagemaker_session.boto_session.client("s3", region_name=region)
+    with open("test/resources/data/files/data.jsonl") as data:
+        body = data.read()
+        s3_client.put_object(Body=body, Bucket=bucket, Key=input_data_key,
+                             ServerSideEncryption='AES256')
+
+    spark.run(
+        submit_app_py="test/resources/code/python/hello_py_spark/hello_py_spark_app.py",
+        submit_py_files=["test/resources/code/python/hello_py_spark/hello_py_spark_udfs.py"],
+        arguments=["--input", input_data_uri, "--output", output_data_uri],
+        configuration={
+            "Classification": "core-site",
+            "Properties": {
+                "fs.s3a.server-side-encryption-algorithm": "AES256"
+            }
+        }
+    )
+    processing_job = spark.latest_job
+
+    waiter = sagemaker_client.get_waiter("processing_job_completed_or_stopped")
+    waiter.wait(
+        ProcessingJobName=processing_job.job_name,
+        # poll every 15 seconds. timeout after 15 minutes.
+        WaiterConfig={"Delay": 15, "MaxAttempts": 60},
+    )
 
     output_contents = S3Downloader.list(output_data_uri, sagemaker_session=sagemaker_session)
     assert len(output_contents) != 0
