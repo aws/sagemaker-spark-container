90dae4bb346148d985d2daf88c2b34ac23671177
Ajay Mahendru <mahendruajay@gmail.com>
2024-02-27 16:15:16 -0800

    release Spark 3.4-cpu-py39-v1.1 (#141)
    Co-authored-by: Ajay Mahendru <mahajay@amazon.com>

--- src/smspark/bootstrapper.py
+++ src/smspark/bootstrapper.py
@@ -72,9 +72,14 @@ class Bootstrapper:
         self.start_spark_standalone_primary()
 
     def copy_aws_jars(self) -> None:
-        self.logger.info("copying aws jars")
+        self.logger.info("copying aws-java-sdk jars")
         for f in glob.glob("/usr/share/aws/aws-java-sdk/*.jar"):
             shutil.copyfile(f, os.path.join(self.JAR_DEST, os.path.basename(f)))
+
+        self.logger.info("copying aws-java-sdk-v2 jars")
+        for f in glob.glob("/usr/share/aws/aws-java-sdk-v2/*.jar"):
+            shutil.copyfile(f, os.path.join(self.JAR_DEST, os.path.basename(f)))
+
         hadoop_aws_jar = self._get_hadoop_jar()
         shutil.copyfile(
             os.path.join(Bootstrapper.HADOOP_PATH, hadoop_aws_jar), os.path.join(self.JAR_DEST, hadoop_aws_jar)
